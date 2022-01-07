35f7df8738079bce7cf40144df298306737912bb
antontokm <ant.tokmakov@gmail.com>
2022-01-07 14:52:56 +0300

    changes models

--- Site_app/models.py
+++ Site_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-class Table(models.Model):
-    text = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.text
+class Driver(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    score = models.IntegerField()
