0fd9ee9620a504f7807807b06f56c2a1e4461f2e
antontokm <ant.tokmakov@gmail.com>
2022-01-11 19:57:12 +0300

    add log out, text on main page

--- Site_app/views.py
+++ Site_app/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 from .models import Driver
 
 def index(request):
-    return render(request, 'Site_app/index.html')
-
-def  drivers(request):
-    drivers = Driver.objects.order_by('date_added')
+    drivers = Driver.objects.order_by('-score')
     context = {'drivers': drivers}
-    return render(request, 'learning_logs/index.html', context)
+    return render(request, 'Site_app/index.html', context)
 
