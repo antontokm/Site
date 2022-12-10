0abe5dd49140bc65447b46b16a375e5b3afef915
antontokm <ant.tokmakov@gmail.com>
2022-12-10 19:15:50 +0300

    update table button done

--- Site_app/api_request.py
+++ Site_app/api_request.py
@@ -2,19 +2,17 @@ import requests
 from .models import Driver
 
 def add_drivers():
+    Driver.objects.all().delete()
     url = "http://ergast.com/api/f1/2022/driverStandings.json"
     headers = {}
     r = requests.get(url, headers=headers)
     response_dict = r.json()
     list_score = response_dict['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings']
-    drivers_count = Driver.objects.count()
-    if len(list_score)!= drivers_count:
-        for i in range(len(list_score)):
-            driver = Driver()
-            driverIndex = list_score[i]
-            driver.first_name = driverIndex["Driver"]['givenName']
-            driver.last_name = driverIndex["Driver"]['familyName']
-            driver.score = float(driverIndex['points'])
-            driver.save()
-            print('test2')
+    for i in range(len(list_score)):
+        driver = Driver()
+        driver.first_name = list_score[i]["Driver"]['givenName']
+        driver.last_name = list_score[i]["Driver"]['familyName']
+        driver.score = float(list_score[i]['points'])
+        driver.save()
+        # print('test2')
 
